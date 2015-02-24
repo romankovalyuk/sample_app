ff93c2e161b82f6f26500583b6577d1b77e499bd
RKovalyuk <your.email@example.com>
2015-02-24 11:00:27 +0200

    added signup controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
 
+  get "user/new"
   root 'home#index'
   match '/contact', to: 'home#contact', via: 'get'
+  match '/signup', to: 'user#new', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
